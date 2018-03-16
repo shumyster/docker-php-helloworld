fbe355eb5870e9f860348991ab776b33cb00b701
Matt Russell <matt.russell33@gmail.com>
2018-03-16 03:07:09 -0700

    index.php 1st update

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 
 echo "Hello, World from Docker! <br>";
+echo "First Jenkins Build <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
